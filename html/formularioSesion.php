d854dbcdb745143a4bdba065773b8f0b647f3f7b
unknown <icalcedo.09play@gmail.com>
2019-05-07 22:34:23 +0200

    Title. Little reupload
    Due to a problem with apache server, i have cloned the repo and reuploaded
    the sing in php file

---
+++ html/formularioSesion.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio de Sesión</title>
+    
+</head>
+<body>
+    
+<div class="formulario">
+<h1>¡Regístrate!</h1>
+  <form action="action_registro.php" method="post">
+      <p><input type="text" name="nombre" size="40" placeholder="Escribe tu nombre"></p>
+      <p><input type="text" name="Apellidos" size="40" placeholder="Escribe tus apellidos"></p>
+      <p><input type="text" name="DNI" size="40" placeholder="Inserta tu DNI"></p>
+      <p><input type="text" name="email" size="40" placeholder="email/gmail/hotmailyahoo"></p>
+      <p><input type="text" name="Usuario" size="40" placeholder="Inventa un usuario chachi"></p>
+      <p><input type="text" name="Contraseña" size="40" placeholder="Escribe tu contraseña super secreta"></p>
+  
+  
+    <input type="submit" value="REGÍSTRATE">
+    
+  
+</form>
+</div>
+     
+</body>
+</html>
