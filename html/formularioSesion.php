d1b13cbcf9d081f949a7f703d366696caa87f0a6
unknown <icalcedo.09play@gmail.com>
2019-05-08 12:59:34 +0200

    Modifications on the sign in form
    Modifications have been done in the css and the php to create the firs prototype
    of the sign in form. not finished yet.

--- html/formularioSesion.php
+++ html/formularioSesion.php
@@ -2,26 +2,26 @@
 <head>
     <meta charset="UTF-8">
     <title>Inicio de Sesión</title>
-    <link rel="stylesheet" type="text/css" href="./signinform.css">
+    <link rel="stylesheet" type="text/css" href="../css/signinform.css">
 </head>
 <body>
     
-<div class="formulario">
-<h1>¡Regístrate!</h1>
+<section id="formulario">
+<p id="titulo">Regístrate</p>
   <form action="action_registro.php" method="post">
-      <p><input type="text" name="nombre" size="40" placeholder="Escribe tu nombre"></p>
-      <p><input type="text" name="Apellidos" size="40" placeholder="Escribe tus apellidos"></p>
-      <p><input type="text" name="DNI" size="40" placeholder="Inserta tu DNI"></p>
-      <p><input type="text" name="email" size="40" placeholder="email/gmail/hotmailyahoo"></p>
-      <p><input type="text" name="Usuario" size="40" placeholder="Inventa un usuario chachi"></p>
-      <p><input type="text" name="Contraseña" size="40" placeholder="Escribe tu contraseña super secreta"></p>
+      <input type="text" id="nombre" name="nombre" size="40" placeholder="Escribe tu nombre">
+      <input type="text" id="Apellidos" name="Apellidos" size="40" placeholder="Escribe tus apellidos">
+      <input type="text" id="DNI" name="DNI" size="40" placeholder="Inserta tu DNI">
+      <input type="mail" id="email" name="email" size="40" placeholder="email/gmail/hotmailyahoo">
+      <input type="text" id="Usuario" name="Usuario" size="40" placeholder="Inventa un usuario chachi">
+      <input type="password" id="Contraseña" name="Contraseña" size="40" placeholder="Escribe tu contraseña super secreta">
   
   
     <input type="submit" value="REGÍSTRATE">
     
   
 </form>
-</div>
+</section>
      
 </body>
 </html>
