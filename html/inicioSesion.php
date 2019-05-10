7fda4e718abcb50b22fd358527f2c4a3794668a9
unknown <icalcedo.09play@gmail.com>
2019-05-10 17:08:58 +0200

    Creation of the log in
    Log in  file created. First version of the file.

---
+++ html/inicioSesion.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <title>¡Inicia Sesión!</title>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed|Overpass" rel="stylesheet">
+</head>
+<body>
+<div class="d-inicio">
+    <button class="btn-main-icon" ><a href="index.html"><img src="../images/speedfooder-icon.png"></a></button>
+</div>
+<section id="formulario">
+<p id="titulo">Regístrate</p>
+  <form action="action_login.php" method="post">
+      
+      <input type="text" id="Usuario" name="Usuario" size="40" placeholder="Inventa un usuario chachi">
+      <input type="password" id="Contraseña" name="Contraseña" size="40" placeholder="Escribe tu contraseña super secreta">
+      
+      <p>¿No tienes usuario? Regístrate <a href="formularioSesion.php">aquí</a></p>
+      <input type="submit" value="¡INICIA SESIÓN!">
+    
+  
+</form>
+</section>
+</body>
+</html>
