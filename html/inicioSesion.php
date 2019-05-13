a62e1b9467d29af55ae5534dcad84dc042225381
unknown <icalcedo.09play@gmail.com>
2019-05-13 18:31:35 +0200

    Add inicioSesion.php
    Added inicioSesion.php and it's correspondent css file.

--- html/inicioSesion.php
+++ html/inicioSesion.php
@@ -3,23 +3,27 @@
     <title>¡Inicia Sesión!</title>
     <meta charset="utf-8">
     <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed|Overpass" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../css/inicioSesion.css">
+                                
+    
 </head>
 <body>
 <div class="d-inicio">
     <button class="btn-main-icon" ><a href="index.html"><img src="../images/speedfooder-icon.png"></a></button>
 </div>
 <section id="formulario">
-<p id="titulo">Regístrate</p>
+<p id="titulo">Inicia Sesión</p>
   <form action="action_login.php" method="post">
       
       <input type="text" id="Usuario" name="Usuario" size="40" placeholder="Inventa un usuario chachi">
       <input type="password" id="Contraseña" name="Contraseña" size="40" placeholder="Escribe tu contraseña super secreta">
       
-      <p>¿No tienes usuario? Regístrate <a href="formularioSesion.php">aquí</a></p>
+      
       <input type="submit" value="¡INICIA SESIÓN!">
     
   
 </form>
+    <p id="AlertaReg">¿No tienes usuario? Regístrate <a href="formularioSesion.php">aquí</a></p>
 </section>
 </body>
 </html>
