3e1bf9de1223f1b72ce755d20c42268d0778170d
beagaliana <bgaliana@protonmail.com>
2019-05-13 18:44:50 +0200

    Add contact form
    Add contact form as well as its style. Also, modified about-us.html in
    order to reference the contact form to href

---
+++ html/formulario-contacto.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>¡Contáctanos!</title>
+    <link rel="stylesheet" type="text/css" href="../css/signinform.css">
+    <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed|Overpass" rel="stylesheet">
+</head>
+<body>
+  <div class="d-inicio">
+    <button class="btn-main-icon" ><a href="index.html"><img src="../images/speedfooder-icon.png"></a></button>
+    
+  </div>
+<section id="formulario">
+<p id="titulo">¡Contáctanos!</p>
+  <form action="action_contacto.php" method="post">
+      <input type="text" id="nombre" name="nombre" size="40" placeholder="Escribe tu nombre">
+      <input type="mail" id="email" name="email" size="40" placeholder="Escribe tu email">
+      <input type="text" id="opinion" name="opinion" size="auto" placeholder="Cuéntanos tu opinión...">
+  
+  
+    <input type="submit" value="Enviar">
+    
+  
+</form>
+</section>
+     
+</body>
+</html>
