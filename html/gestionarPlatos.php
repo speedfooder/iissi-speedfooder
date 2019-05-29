246bc98cb21d8c31a6ecc5e6b5448d9c026398eb
Bea Galiana <bgaliana@protonmail.com>
2019-05-29 10:20:45 +0200

    Add fuctions to edit and delete dishes
    These two methods allow to edit info about the dishes and to delete
    them. They are only going to be callable by the admin.

--- html/gestionarPlatos.php
+++ html/gestionarPlatos.php
@@ -20,4 +20,27 @@ function consulta_alimentos( $conn, $query)
 		header("Location: excepcion.php");
 	}
 } 
+
+function elimina_plato($conexion,$idplato) {
+	try {
+		$stmt=$conexion->prepare('CALL ELIMINA_PLATO(:idplato)');
+		$stmt->bindParam(':idplato',$idplato);
+		$stmt->execute();
+		return "";
+	} catch(PDOException $e) {
+		return $e->getMessage();
+    }
+}
+
+function actualiza_plato($conexion,$idplato,$nombrePlato) {
+	try {
+		$stmt=$conexion->prepare('CALL ACTUALIZA_PLATO(:idplato,:nombrePlato)');
+		$stmt->bindParam(':idplato',$idplato);
+		$stmt->bindParam(':nombrePlato',$nombrePlato);
+		$stmt->execute();
+		return "";
+	} catch(PDOException $e) {
+		return $e->getMessage();
+    }
+}
 ?>
