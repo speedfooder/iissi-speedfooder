a3c63a2042ed2c83456c17824af0ed993bc9d7cc
IgnacioCVGitHub <icalcedo.09play@gmail.com>
2019-06-13 13:41:31 +0200

    Creado formulario de baja de usuario
    Creado todo lo respectivo a la baja de usuario en cuanto a css y html se refiere. queda implementar el php correspondiente

---
+++ html/bajausuario.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+    <title>Baja de usuario</title>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed|Overpass" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../css/bajausuario.css">
+</head>
+<body>
+<div class="d-inicio">
+    <button class="btn-main-icon" ><a href="index.html"><img src="../images/speedfooder-icon.png"></a></button>
+</div>
+<section id="formulario">
+<p id="titulo">Este es el adios...</p>
+
+<p id="descript">Lamentamos que te tengas que ir, pero si así lo deseas, rellena los campos necesarios</p>
+  <form action="actionbaja.php" method="post">
+      
+
+      <input type="text" id="Usuario" name="usuario" size="40" placeholder="Escribe aquí tu usuario">
+      <input type="password" id="Contraseña" name="contrasena" size="40" placeholder="Escribe tu contraseña">
+
+      
+      
+      <input type="submit" name="submit" value="¡Nos vemos pronto!">
+    
+  
+</form>
+</section>
+</body>
+</html>
