b2b8b4978cf50236a634ecc562384e020c6dbfdc
Bea Galiana <bgaliana@protonmail.com>
2019-06-26 14:55:01 +0200

    Add server val on delete user form
    Fix typo on server val

--- html/bajausuario.php
+++ html/bajausuario.php
@@ -33,16 +33,31 @@
     <meta charset="utf-8">
     <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed|Overpass" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="../css/bajausuario.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jQuery/1.12.4/jQuery.min.js"></script>
 </head>
 <body>
 <div class="d-inicio">
     <button class="btn-main-icon" ><a href="index.php"><img src="../images/speedfooder-icon.png"></a></button>
+
+	<?php 
+		// Mostrar los erroes de validación (Si los hay)
+		if (isset($errores) && count($errores)>0) { 
+	    	echo "<div class=\"error\">";
+			echo "<h4> Errores en el formulario:</h4>";
+    		foreach($errores as $error){
+    			echo $error;
+			} 
+    		echo "</div>";
+  		}
+	?>
+
 </div>
+
 <section id="formulario">
 <p id="titulo">Este es el adios...</p>
 
 <p id="descript">Lamentamos que te tengas que ir, pero si así lo deseas, rellena los campos necesarios</p>
-  <form action="accion_baja_usuario.php" method="post">
+  <form action="validacion_server_borrar_user.php" method="post">
       
 
       <input type="text" id="Usuario" name="usuario" size="40" placeholder="Escribe aquí tu usuario">
