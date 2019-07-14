19246c89e7b98eb53251bdaf0843627451d50cdd
Bea Galiana <bgaliana@protonmail.com>
2019-07-14 22:47:54 +0200

    Add wiki search on allergens
    Also fix css

--- html/alergeno.php
+++ html/alergeno.php
@@ -6,6 +6,15 @@ require_once ("gestionBD.php");
 require_once ("gestionarPlatos.php");
 require_once ("paginacion_consulta.php");
 
+if (isset($_SESSION["alimento"])) {
+	$alimento1 = $_SESSION["alimento"];
+	unset($_SESSION["alimento"]);
+	$_SESSION["alimentotrabajo"]=$alimento1;
+}else if (isset($_SESSION["alimentotrabajo"])){
+	$alimento1=$_SESSION["alimentotrabajo"];
+}else{
+	header ("Location: alimento.php");
+}
 if (isset($_SESSION["paginacion"])){
 		$paginacion = $_SESSION["paginacion"];
 }
@@ -13,11 +22,6 @@ if (isset($_SESSION["opcion"])){
 	$option=$_SESSION["opcion"];
 	unset($_SESSION["opcion"]);
 }
-if (isset($_SESSION["alimento"])) {
-	$alimento=$_SESSION["alimento"];
-	unset($_SESSION["alimento"]);
-}
-
 $pagina_seleccionada = isset($_GET["PAG_NUM"]) ? (int)$_GET["PAG_NUM"] : (isset($paginacion) ? (int)$paginacion["PAG_NUM"] : 1);
 	$pag_tam = isset($_GET["PAG_TAM"]) ? (int)$_GET["PAG_TAM"] : (isset($paginacion) ? (int)$paginacion["PAG_TAM"] : 5);
 
@@ -33,7 +37,7 @@ unset($_SESSION["paginacion"]);
 $conexion = crearConexionBD();
 
 $query=	"SELECT DISTINCT IDALERGENO,NOMBREALERGENO FROM ALIMENTOS,ALERGENOS WHERE alergenos.idALERGENO="
-	. $alimento["ALERGENO"];
+	. $alimento1["ALERGENO"];
 		
 							
 
@@ -123,7 +127,10 @@ $filas = consulta_paginada($conexion, $query, $pagina_seleccionada, $pag_tam);
 	?>
 
 
-
+	<div>
+		<h2 class="menu-section-title">El <?php echo $alimento1["NOMBREALIMENTO"] ?> contiene el siguiente alérgeno:</h2>
+	</div>
+	
 	<article >
 
 		<form method="post" class="plato" action="controlador_alergenos.php">
@@ -138,22 +145,28 @@ $filas = consulta_paginada($conexion, $query, $pagina_seleccionada, $pag_tam);
 				type="hidden" value="<?php echo $fila["NOMBREALERGENO"]; ?>"/>
 		
 		<table>
-				<tr>
-				<th class="columnas">
-
-						<!-- mostrando alimento -->
-						<div><b><?php echo $fila["NOMBREALERGENO"]; ?></b></div>
-				</th>
-				</th>
-				<th class="c-icono">
-							<?php if (isset($_SESSION['login']) && $_SESSION['login'] =='admin') {?>
-								<!--<button id="delete" name="deleteAlergeno" type="submit">
-									<img src="../images/ic-delete.png">
-								</button> -->
-							<?php }?>
-						</th>
-					</tr>	
-			</table>	
+			
+			<!-- mostrando alimento -->
+			<tr id="alerg"><h3><?php echo $fila["NOMBREALERGENO"]; ?></h3></tr>
+			<tr>
+				<div  id="wiki">
+				<h3>Aquí puedes consultar más información acerca de <?php echo $fila["NOMBREALERGENO"]; ?></h3>
+				<!– Búsqueda wikipedia –>
+				<center>
+				<a title="Wikipedia" href="http://es.wikipedia.org/"><img src=http://upload.wikimedia.org/wikipedia/commons/0/04/Lemon_wiki_banner.jpg width="200" alt="logo de la wikipedia" /></a>
+				<form action="http://es.wikipedia.org/w/index.php?title=Especial:Buscar&search=" id="searchform" target="blank">
+				<table bgcolor="#FFFFFF"><tr><td>
+				<input  type="hidden" value="0" name="redirs" />
+				<input class="search-wiki" size="17" value="" name="search" placeholder="Búsqueda" autocomplete="off" />
+				<input class="search-wiki" type="hidden" value="Search" name="fulltext" />
+				<input class="search-wiki" type="submit" value="Buscar" />
+				</td></tr></table>
+				</form>
+				</center>
+				</div>
+			</tr>
+		</table>
+		
 		</form>
 	</article >		
 
