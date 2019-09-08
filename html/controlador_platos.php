34426466ba7019b5080df7aaaa64ca5cc7bd8d37
IgnacioCVGitHub <icalcedo.09play@gmail.com>
2019-09-08 12:51:47 +0200

    Edited controlador to make it consider edition cancel

--- html/controlador_platos.php
+++ html/controlador_platos.php
@@ -20,7 +20,15 @@
 		else if (isset($_REQUEST["delete"])) Header("Location: accion_borrar_plato.php");
 		else if (isset($_REQUEST["grabar"])) Header("Location: accion_modificar_plato.php");
 		else if (isset($_REQUEST["grabarprecio"])) Header("Location: accion_modificar_precio_plato.php");
-		else if (isset($_REQUEST["delete"])) Header("Location: accion_borrar_plato.php");
+		else if (isset($_REQUEST["delete"])){
+			if(isset($_SESSION["opcion"])){
+				unset($_SESSION["opcion"]);
+				Header("Location: menu.php");
+			}
+			else{
+				Header("Location: accion_borrar_plato.php");
+			}
+		} 
 	}
 	else 
 		Header("Location: menu.php");
